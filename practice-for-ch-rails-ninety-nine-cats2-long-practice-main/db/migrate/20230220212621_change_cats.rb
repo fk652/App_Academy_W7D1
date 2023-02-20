cff771a99bd79ab26c9ebefd5c39b85e2a4bd9a7
Charles Cruse <charle9@vt.edu>
2023-02-20 16:33:49 -0500

    change cats

---
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/db/migrate/20230220212621_change_cats.rb
@@ -0,0 +1,7 @@
+class ChangeCats < ActiveRecord::Migration[7.0]
+  def change
+    add_column :cats, :owner_id, :bigint
+    add_index :cats, :owner_id
+    add_foreign_key :cats, :users, column: :owner_id 
+  end
+end
