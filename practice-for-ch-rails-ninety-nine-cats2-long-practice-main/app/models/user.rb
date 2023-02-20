0ac9009688f74110b1d496ac06e87c3dd2d6baef
Fahim Khan <fk652@nyu.edu>
2023-02-20 14:44:30 -0500

    create User migration and model

---
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
@@ -0,0 +1,53 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+
+  attr_reader :password
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
+  end
+
+  def is_password?(password)
+    bcrypt_obj = BCrypt::Password.new(self.password_digest)
+    bcrypt_obj.is_password?(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    if user && user.is_password?(password)
+      return user
+    else
+      return nil
+    end
+  end
+
+  private
+  def generate_unique_session_token
+    token = SecureRandom::urlsafe_base64
+    while User.exists?(session_token: token)
+      token = SecureRandom::urlsafe_base64
+    end
+    token
+  end
+
+  def reset_session_token
+    self.session_token = generate_unique_session_token
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+
+  end
+end
