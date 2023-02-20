25ecbfeeb77aebcc8576a3fa578bfa98c8ae0ab8
Charles Cruse <charle9@vt.edu>
2023-02-20 15:46:37 -0500

    added to new.html.erb

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/application_controller.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/application_controller.rb
@@ -19,4 +19,12 @@ class ApplicationController < ActionController::Base
         session[:session_token] = nil
         @current_user = nil
     end
+
+    def require_logged_in
+        redirect_to new_session_url unless logged_in?
+    end
+
+    def require_logged_out
+        redirect_to users_url if logged_in?
+    end
 end
--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/users_controller.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/users_controller.rb
@@ -1,3 +1,26 @@
 class UsersController < ApplicationController
-    
+    before_action :require_logged_out, only: [:new, :create]
+
+    def new
+        render :new
+    end
+
+    def create
+
+        @user = User.new(user_params)
+
+        if @user.save
+            login(@user)
+            redirect_to cats_url
+        else
+            puts @user.errors.full_messages
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
